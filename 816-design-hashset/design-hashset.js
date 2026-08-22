d637d184f87bc84b0667ec21bd8a6c65f1c37cdd
hamzaKhan2004 <145287096+hamzaKhan2004@users.noreply.github.com>
2026-08-22 18:17:32 +0530

    Time: 34 ms (56.76%) | Memory: 69.1 MB (56.08%) - LeetSync

---
+++ 816-design-hashset/design-hashset.js
@@ -0,0 +1,37 @@
+var MyHashSet = function() {
+    this.set = [];
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function(key) {
+    if (!this.set[key]) {
+        this.set[key] = true;
+    }
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function(key) {
+    this.set[key] = false;
+};
+
+/** 
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function(key) {
+    return this.set[key] === true;
+};
+
+/** 
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
